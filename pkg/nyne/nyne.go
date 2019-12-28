01c75099233de1fd8ccc4c410da3a6e4c19380af
Daniel <daniel@martin.ai>
2019-12-28 11:34:57 -0500

    tab and indent presets now implemented

--- pkg/nyne/nyne.go
+++ pkg/nyne/nyne.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"unicode/utf8"
+	"fmt"
 
 	"9fans.net/go/acme"
 	"git.sr.ht/~danieljamespost/nyne/pkg/nyne/golang"
@@ -29,44 +31,85 @@ func New(conf *config.Config) {
 		case "new":
 			runFmt(conf, event)
 		}
-		if event.Name != "" && event.Op == "put" {}
 	}
 }
 
-func runFmt(conf *config.Config, event *acme.Event) {
+func runFmt(conf *config.Config, event acme.LogEvent) {
 	for _, spec := range conf.Spec {
-		for idx, ext := range spec.Ext {
+		for _, ext := range spec.Ext {
 			if strings.HasSuffix(event.Name, ext) {
-				fmt(event.ID, spec[idx].Fmt)
+				format(event, spec.Fmt)
 			}
 	    	}
     	}
 }
 
-func runCmd(conf *config.Config, event *acme.Event) {
+func runCmd(conf *config.Config, event acme.LogEvent) {
 	for _, spec := range conf.Spec {
 		for _, ext := range spec.Ext {
 			if strings.HasSuffix(event.Name, ext) {
 				for _, cmd := range spec.Cmd {
 					args := replaceName(cmd.Args, event.Name)
 					reformat(event.ID, event.Name, cmd.Exec, args, ext)
-			    		event, err = l.Read()
-			    		if err != nil {
-			    			log.Fatal(err)
-			    		}
 		    		}
 		    	}
 	    	}
     	}
 }
 
-func fmt(event *acme.Event, f config.Format) {
+func format(event acme.LogEvent, f config.Format) {
 	w, err := acme.Open(event.ID, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return
 	}
 	defer w.CloseFiles()
-
+	
+	if f.Indent != 0 {
+		tabCmd := fmt.Sprintf("Tab %d", f.Indent)
+		if err := w.Ctl("cleartag"); err != nil {
+			log.Print(err)
+		}
+		tag, err := w.ReadAll("tag")
+		if err != nil {
+			log.Print(err)
+		}
+		offset := utf8.RuneCount(tag)
+		cmdlen := utf8.RuneCountInString(tabCmd)
+		if err := w.Fprintf("tag", "%s", tabCmd); err != nil {
+			log.Print(err)
+		}
+		evt := new(acme.Event)
+		evt.C1 = 'M'
+		evt.C2 = 'x'
+		evt.Q0 = offset
+		evt.Q1 = offset + cmdlen
+		w.WriteEvent(evt)		
+	}
+	
+	if f.Expand == true {
+		expCmd := "Tabexpand"
+		if err := w.Ctl("cleartag"); err != nil {
+			log.Print(err)
+		}
+		tag, err := w.ReadAll("tag")
+		if err != nil {
+			log.Print(err)
+		}
+		offset := utf8.RuneCount(tag)
+		cmdlen := utf8.RuneCountInString(expCmd)
+		if err := w.Fprintf("tag", "%s", expCmd); err != nil {
+			log.Print(err)
+		}
+		evt := new(acme.Event)
+		evt.C1 = 'M'
+		evt.C2 = 'x'
+		evt.Q0 = offset
+		evt.Q1 = offset + cmdlen
+		w.WriteEvent(evt)		
+	}
+	
+	return
 }
 
 func replaceName(arr []string, name string) []string {
@@ -88,10 +131,10 @@ func reformat(id int, name string, x string, args []string, ext string) {
 		return
 	}
 	defer w.CloseFiles()
-
+	
 	old, err := ioutil.ReadFile(name)
 	if err != nil {
-			return
+		return
 	}
 	new, err := exec.Command(x, args...).CombinedOutput()
 	if err != nil {
